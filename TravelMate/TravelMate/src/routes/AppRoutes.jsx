dd818739ef071213145370e5e54ae95d409d02b4
Divya <divyaa7u@gmail.com>
2026-08-21 21:55:51 +0545

    Add booking details and my bookings

--- TravelMate/TravelMate/src/routes/AppRoutes.jsx
+++ TravelMate/TravelMate/src/routes/AppRoutes.jsx
@@ -8,38 +8,52 @@ import MyBookings from "../pages/MyBookings";
 import DestinationDetails from "../components/destinations/DestinationDetails/DestinationDetails";
 import Booking from "../components/booking/Booking";
 
+import BookingDetails from "../pages/BookingDetails/BookingDetails";
+
 const AppRoutes = () => {
   return (
     <Routes>
 
+      {/* Home */}
       <Route
         path="/"
         element={<Home />}
       />
 
+      {/* About */}
       <Route
         path="/about"
         element={<About />}
       />
 
+      {/* Destinations */}
       <Route
         path="/destinations"
         element={<Destinations />}
       />
 
+      {/* Destination Details */}
       <Route
         path="/destinations/:id"
         element={<DestinationDetails />}
       />
 
+      {/* Booking */}
       <Route
         path="/booking/:id"
         element={<Booking />}
       />
 
+      {/* My Bookings */}
       <Route
         path="/my-bookings"
         element={<MyBookings />}
+      />
+
+      {/* Booking Details */}
+      <Route
+        path="/booking-details/:id"
+        element={<BookingDetails />}
       />
 
     </Routes>
