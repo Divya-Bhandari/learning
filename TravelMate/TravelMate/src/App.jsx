cf03572a4b0d002b68d8b8375b9e373ed566806c
Divya <divyaa7u@gmail.com>
2026-09-08 21:44:35 +0545

    changes are made

--- TravelMate/TravelMate/src/App.jsx
+++ TravelMate/TravelMate/src/App.jsx
@@ -1,5 +1,5 @@
-import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer";
+import Navbar from "./components/layout/Navbar/Navbar";
+import Footer from "./components/layout/Footer/Footer";
 import AppRoutes from "./routes/AppRoutes";
 
 function App() {
